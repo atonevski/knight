83b7c4d7e8a0cac72b3ffb7106f7e099e6547cf3
Andreja Tonevski <atonevski@gmail.com>
2017-11-01 21:14:43 +0100

    Added kight moves modifier R|V|H|C

--- src/knight.cr
+++ src/knight.cr
@@ -5,9 +5,8 @@ module Knight
 
   def self.print_syntax_and_exit
     puts <<-EOS
-      usage knight [[size] col row]
-      \twhere:
-      \t\tcol/row start position: col: A|B|C..., row 1|2|3...
+      usage knight [[size] col row] [N|R|V|H|C]
+      where: col/row start position, col: A|B|C..., row 1|2|3...
     EOS
     exit 1
   end
@@ -15,22 +14,58 @@ module Knight
   class Board
     MAX = 10
     @@alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    #   x  
+    #  .->   . o . o .    . 1 . 2 .
+    # y|     o . . . o    8 . . . 3
+    #  V     . . x . .    . . x . .
+    #        o . . . o    7 . . . 4
+    #        . o . o .    . 6 . 5 .
+    #     
     @@diffs = [
-      { x: -1, y: -2 },
-      { x:  1, y: -2 },
-      { x:  2, y: -1 },
-      { x:  2, y:  1 },
-      { x:  1, y:  2 },
-      { x: -1, y:  2 },
-      { x: -2, y: -1 },
-      { x: -2, y:  1 },
+      { x: -1, y: -2 }, # 1
+      { x:  1, y: -2 }, # 2
+      { x:  2, y: -1 }, # 3
+      { x:  2, y:  1 }, # 4
+      { x:  1, y:  2 }, # 5
+      { x: -1, y:  2 }, # 6
+      { x: -2, y:  1 }, # 7
+      { x: -2, y: -1 }, # 8
     ]
+    # rotate 1 click clock-wise: 1=>8, 2=>1, 3=>2,... 7=>6, 8=>7
+    # vertical reflection: 1=>2, 2=>1, 3=>8, 4=>7, 5=>6, 6=>5, 7=>4, 8=>3
+    # horizontal reflection: 1=>6, 2=>5, 3=>4, 4=>3, 5=>2, 6=>1, 7=>8, 8=>7
+    # central reflection: 1=>5, 2=>6, 3=>7, 4=>8, 5=>1, 6=>2, 7=>3, 8=>4
+    def diffs_rot()
+      return [ 
+        @@diffs[7], @@diffs[0], @@diffs[1], @@diffs[2],
+        @@diffs[3], @@diffs[4], @@diffs[5], @@diffs[6],
+      ]
+    end
+    def diffs_vert()
+      return [ 
+        @@diffs[1], @@diffs[0], @@diffs[7], @@diffs[6],
+        @@diffs[5], @@diffs[4], @@diffs[3], @@diffs[2],
+      ]
+    end
+    def diffs_hor()
+      return [ 
+        @@diffs[5], @@diffs[4], @@diffs[3], @@diffs[2],
+        @@diffs[1], @@diffs[0], @@diffs[7], @@diffs[6],
+      ]
+    end
+    def diffs_cent()
+      return [ 
+        @@diffs[4], @@diffs[5], @@diffs[6], @@diffs[7],
+        @@diffs[0], @@diffs[1], @@diffs[2], @@diffs[3],
+      ]
+    end
 
     @one_halve : Int32
     @two_thirds : Int32
     @three_fourths : Int32
+    @diffs : Array(NamedTuple(x: Int32, y: Int32))
 
-    def initialize(@size : Int32, x, y : Int32)
+    def initialize(@size : Int32, x, y : Int32, diffs = 'N')
       raise "Size #{ @size } too big" if @size > MAX
       if x < 0 || y < 0 || x >= @size || y >= @size
         raise "Invalid start position (#{x}, #{y})"
@@ -43,6 +78,14 @@ module Knight
       @one_halve = @size*@size/2
       @two_thirds = @size*@size*2/3
       @three_fourths = @size*@size*3/4
+      @diffs = case diffs
+               when 'N' then @@diffs
+               when 'R' then diffs_rot
+               when 'V' then diffs_vert
+               when 'H' then diffs_hor
+               when 'C' then diffs_cent
+               else [ ] of NamedTuple(x: Int32, y: Int32)
+               end
     end
 
     def try(level, x, y) : Bool
@@ -63,7 +106,7 @@ module Knight
         end
       end
 
-      @@diffs.each do |d|
+      @diffs.each do |d|
         xdx = x + d[:x]
         ydy = y + d[:y]
         if xdx >= 0 && xdx < @size &&
@@ -83,7 +126,7 @@ module Knight
     def is_reachable?(x, y)
       return true if @table[x][y] != 0
 
-      @@diffs.each do |d|
+      @diffs.each do |d|
         xdx = x + d[:x]
         ydy = y + d[:y]
         if xdx >= 0 && xdx < @size &&
@@ -116,6 +159,8 @@ module Knight
   x = 6
   y = 6
   n = 8
+  d = 'N'
+  d = (ARGV.pop)[0].upcase if ARGV[-1] =~ /N|R|V|H|C/i
   case ARGV.size
     when 0
     when 2
@@ -134,20 +179,8 @@ module Knight
       print_syntax_and_exit
   end
   start = Time.new
-  board = Board.new n, x, y
+  board = Board.new n, x, y, d
   board.try 1, x, y
   board.print
   puts "elapsed time: #{ Time.now - start }"
 end
-
-# nn: nn sec, nm: n min, nM: n0 min, nh: n hour, nH: n0 hours
-#
-#  r\c  A  B  C  D  E  F  G  H
-#  1           
-#  2    6     1m    2    22 
-#  3         27          12
-#  4                  12m21
-#  5   
-#  6    4
-#  7       3m             1
-#  8   
